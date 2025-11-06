ce27b5dd8f2f38cab4fdab8f3b25fca910fb3781
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2025-11-06 11:34:26 +0530

    react

--- ABES-CSE-C(2025-26)/frontend/adi/src/App.jsx
+++ ABES-CSE-C(2025-26)/frontend/adi/src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
-import Card from './Component/Card';
-import Count1 from './Component/Count1';
-import Nabvar from './Component/Nabvar';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react'
+import Adi from './Component/Adi'
+
 function App() {
   return (
     <div>
-      <Count1 />
+      <Adi />
     </div>
   )
 }
+
 export default App
---
+++ ABES-CSE-C(2025-26)/frontend/adi/src/Component/Adi.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+function Adi() {
+
+    function Home() {
+        return <h1>This is my Home Page</h1>
+    }
+
+    function About() {
+        return <h1>This is my About Page</h1>
+    }
+
+    return (
+        <div>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+
+            </nav>
+
+            <Routes>
+                <Route path="/" element={Home} />
+                <Route path="/about" element={About} />
+
+            </Routes>
+
+
+        </div>
+    )
+}
+
+export default Adi
