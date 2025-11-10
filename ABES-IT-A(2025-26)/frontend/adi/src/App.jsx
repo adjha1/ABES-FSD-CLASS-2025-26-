a3cc9b2c7941bb0fb9208c6daea338ebd16704d1
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2025-11-10 12:48:02 +0530

    react

--- ABES-IT-A(2025-26)/frontend/adi/src/App.jsx
+++ ABES-IT-A(2025-26)/frontend/adi/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Card from './components/Card'
-import Navbar from './components/Navbar'
+import Link1 from './components/Link1'
+
 function App() {
   return (
     <div>
-
+      <Link1 />
     </div>
   )
 }
+
 export default App
