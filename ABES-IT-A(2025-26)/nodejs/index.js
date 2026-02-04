aa29e65ca076f5c1a3dfe2e160d4937e1adfdd38
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2026-02-04 10:10:51 +0530

    nodejs

---
+++ ABES-IT-A(2025-26)/nodejs/index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const myserver = http.createServer((req, res) => {
+    // console.log('server1');
+    // res.end('HI tihs is my first server');
+
+    if (req.url == '/') {
+        res.end("this tis my home page")
+    }
+    else if (req.url == '/about') {
+        res.end("this is my about page ")
+    }
+    else {
+        res.end("404 page is not found")
+    }
+});
+
+
+myserver.listen(8000, () => console.log('server is run'))
