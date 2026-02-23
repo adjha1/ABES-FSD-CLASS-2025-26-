eecfcc9fc03eda677cf713ad5610bb414b656b86
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2026-02-23 09:59:33 +0530

    nodejs

--- ABES-CE(2025-26)/nodejs/index.js
+++ ABES-CE(2025-26)/nodejs/index.js
@@ -51,17 +51,69 @@
 
 
 
-const os = require('os');
+// const os = require('os');
 
-console.log("platform", os.platform());
-console.log("user", os.userInfo);
+// console.log("platform", os.platform());
+// console.log("user", os.userInfo);
 
-console.log("CPU", os.arch());
+// console.log("CPU", os.arch());
 
-console.log("free memory", os.freemem(), "bytes");
+// console.log("free memory", os.freemem(), "bytes");
 
-console.log("total memory", os.totalmem(), "bytes");
+// console.log("total memory", os.totalmem(), "bytes");
 
-console.log("system update", os.uptime());
+// console.log("system update", os.uptime());
 
-console.log("host name", os.hostname());
+// console.log("host name", os.hostname());
+
+
+// const http = require('http');
+// const fs = require('fs');
+
+// const home = fs.readFileSync('./a.html');
+// const myserver = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.end(home);
+//     //     res.setHeader('Contant-type', 'text/plain');
+//     //     res.end("hello wrold");
+// })
+// myserver.listen(8000, () => console.log("server is run"))
+
+// 
+
+// const users= require('./a');
+// console.log(users);
+
+const fs = require("fs");
+
+/* const temp =
+{
+    name: "xyz",
+    age: 52
+}
+
+fs.writeFile('./user.json', JSON.stringify(temp), (err) => {
+    if (err) throw err;
+    console.log("Done");
+}) */
+const users = require('./user.json');
+const user = fs.readFile(users);
+let data = JSON.parseData(user);
+const temp =
+{
+    name: "abc",
+    age: 32
+}
+data.push(temp);
+// fs.writeFile(users, JSON.stringify(data), (err) => {
+//     if (err){
+//         console.log(err);
+//         return
+//     }
+//     console.log("DONE");
+// })
+
+function deleteUser(id) {
+    let users = readData();
+
+}
--- ABES-DS-B(2025-26)/nodejs/index.js
+++ ABES-DS-B(2025-26)/nodejs/index.js
@@ -29,13 +29,25 @@
 
 
 
-const os = require('os');
+// const os = require('os');
 
-console.log("system platform", os.platform());
-console.log("user info", os.userInfo());
-console.log("cpu", os.arch());
-console.log("free memory", os.freemem());
-console.log("total memory", os.totalmem());
-console.log("uptime", os.uptime());
-console.log("homedir", os.homedir());
-console.log("host name", os.hostname());
+// console.log("system platform", os.platform());
+// console.log("user info", os.userInfo());
+// console.log("cpu", os.arch());
+// console.log("free memory", os.freemem());
+// console.log("total memory", os.totalmem());
+// console.log("uptime", os.uptime());
+// console.log("homedir", os.homedir());
+// console.log("host name", os.hostname());
+
+
+
+const http = require('http');
+const fs = require('fs');
+
+const home = fs.readFileSync('./a.html');
+const myserver = http.createServer((req, res) => {
+    res.end(home)
+})
+
+myserver.listen(8000, () => console.log("server is run"));
